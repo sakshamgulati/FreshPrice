0033fa55507aacae5a9e979ea59f34fc07055da3
sakshamgulati <sakshamgulati123@gmail.com>
2021-10-29 00:41:22 -0400

    Finished git skeleton

---
+++ src/DataOps/ImageCrawler.py
@@ -0,0 +1,4 @@
+import selenium
+
+DRIVER_PATH = 'C:\Users\saksh\PycharmProjects\FreshPrice\FreshPrice\Resources\Selenium\chromedriver.exe'
+wd = webdriver.Chrome(executable_path=DRIVER_PATH)
---
+++ src/DataOps/__init__.py
@@ -0,0 +1,1 @@
+from DataOps.ImageCrawler import *
